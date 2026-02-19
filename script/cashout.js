350bf44e7130751e596507dd97b625f68a32dacd
Ananya Chakraborty Gourab <aborty174@gmail.com>
2026-02-19 23:07:03 +0600

    Design Cashout Form

---
+++ script/cashout.js
@@ -0,0 +1,9 @@
+document.getElementById("cashout-pin").addEventListener("click",function(){
+    //get the agent number and validation
+    //get the amount, validate, connect to number 
+    //get the current balance, validate, connect to number
+    //calculate new balance
+    //get the UI and verify
+    //true:: show an alert > set balance
+    //false:: show error > null 
+})
